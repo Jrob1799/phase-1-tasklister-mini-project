6cd29669c1258e6cb5a96fa58153750b9b76d950
Jacob <jacobwroberts33@gmail.com>
2023-04-12 14:29:10 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const newTask = document.createElement("li");
+    const taskDescription = document.querySelector("#new-task-description")
+    newTask.textContent = taskDescription.value;
+    taskList.appendChild(newTask);
+    taskDescription.value = ""
+
+    // create delete button for new task
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    newTask.appendChild(deleteButton);
+
+    // add event listener to delete button
+    deleteButton.addEventListener("click", () => {
+      newTask.remove();
+    });
+  })
+})
